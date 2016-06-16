4a9ad70bc991fbda380b9ac3c035bf2a27678101
ytbau <ytbau@mmu.edu.my>
2016-06-16 21:04:00 +0800

    Add files via upload

---
+++ sequence_structure_important_else_error.py
@@ -0,0 +1,9 @@
+# sequence structure is important
+# error in code because order() function is not defined
+
+order(3, 5); # print 3 5
+
+order(b=3, a=5); # print 5 3
+
+def order(a, b):
+    print a, b;
