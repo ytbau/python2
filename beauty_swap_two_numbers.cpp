b9937ecca27c162d5b2de7a66eff1187bc173adf
ytbau <ytbau@mmu.edu.my>
2016-06-28 00:24:42 +0800

    Add files via upload

---
+++ beauty_swap_two_numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 66;
+    int b = 99;
+    cout << a << " " << b << endl;
+    swap(a, b); //English-like line
+    cout << a << " " << b << endl;
+
+    return 0;
+}
+
+//a = 66
+//
+//b = 99
+//
+//print a, b # print 66 99
+//
+//a, b = b, a
+//
+//print a, b # print 99 66
