a08a7cc2010fb34f179a60dacc5863015603c0a8
ytbau <ytbau@mmu.edu.my>
2016-06-28 23:46:55 +0800

    Add files via upload

---
+++ no switch-case statement.py
@@ -0,0 +1,17 @@
+# no switch-case statement in Python
+
+char key = 'z';
+
+switch( key ) 
+{
+    case 'a':
+        result = 1;
+        break;
+    case 'b':
+        result = 2;
+        break;
+    default :
+        result = -1;
+}
+
+print result;
